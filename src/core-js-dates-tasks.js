e907b70d33d866449d863854612b9b72b36b0605
Yulya Is <ishimyua@gmail.com>
2024-03-03 23:12:55 +0600

    feat: add solution task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -87,8 +87,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -102,8 +102,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dateStart1 = new Date(dateStart);
+  const dateEnd1 = new Date(dateEnd);
+  const days = dateEnd1.getTime() - dateStart1.getTime();
+  return Math.round(days / (1000 * 3600 * 24) + 1);
 }
 
 /**
