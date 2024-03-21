9a0cc0afe1865108693d2c6000ca43344933c241
Yulya Is <ishimyua@gmail.com>
2024-03-21 20:46:28 +0600

    feat: add solution task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -213,8 +213,11 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const date1 = new Date(date.getFullYear());
+  const date2 = new Date(date1);
+  if (date2.getDay() === 5 && date2.getDate() === 13);
+  return date2;
 }
 
 /**
